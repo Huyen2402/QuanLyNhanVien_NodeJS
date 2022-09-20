b4e0b7302e3a85b58643c62cd829958469afc728
Huyen2402 <huyenb1910384@student.ctu.edu.vn>
2022-09-20 10:36:21 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require("express");
-const cors = require("cors") ;
+const cors = require("cors");
+const ApiError = require("./app/api-error");
 const { json } = require("express");
 const app = express();
 const contactRouter = require("./app/route/contact.route");
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactRouter);
-app.get("/",(req, res ) =>{
-    res.json({message: "Welcome to contact book application."});
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to contact book application." });
 });
+app.use((req, res, next) => {
+
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+       
+    });
+});
+
 module.exports = app;
